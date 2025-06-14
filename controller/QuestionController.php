5aa820279830c60c1ff625f793097fea921a4fb7
SergioDanRam <sergiodanramirez@gmail.com>
2025-06-14 18:42:26 -0300

    creando metodos

--- controller/QuestionController.php
+++ controller/QuestionController.php
@@ -11,5 +11,29 @@ class QuestionController extends BaseController
         $this->view = $view;
     }
 
+    public function show(){
+
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        $preguntasSugeridas =$this->model->obtenerPreguntasSugeridas();
+        echo "<pre>";
+        var_dump($preguntasSugeridas);
+        echo "</pre>";
+        echo "\n\n\nnashe";
+
+        $this->view->render('questionsSuggested', $preguntasSugeridas);
+    }
+
+    public function aprobarPregunta(){
+        $this->view->render('preguntasSugeridas');
+
+    }
+
+    public function rechazarPregunta(){
+        $this->view->render('preguntasSugeridas');
+    }
+
 
 }
