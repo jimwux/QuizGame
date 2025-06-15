cd8f23802d7bfe740b97837911b2e23be631c613
TobiasTejerina <tobias.tejerina@gmail.com>
2025-06-15 17:41:06 -0300

    commit para guardarReporte (no probado, es para crear la rama)

--- model/QuestionModel.php
+++ model/QuestionModel.php
@@ -8,5 +8,16 @@ class QuestionModel
     {
         $this->database = $database;
     }
+public function guardarReporte($idUsuario,$idPregunta,$motivo)
+{
+    if ($idUsuario !== null && $idPregunta !== null && $motivo !== null) {
+        $sql = "INSERT INTO reporte_pregunta 
+                    (id_pregunta, id_usuario, motivo, fecha_reporte, estado) 
+                    VALUES (?, ?, ?, NOW(), false)";
 
+        if ($this->database->execute($sql, [$idPregunta, $idUsuario, $motivo]))
+            return true;
+            }
+            return false;
+}
 }
