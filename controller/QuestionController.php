1dc86f5fb91e0644aeba662ebc390b640ca1b3d3
TobiasTejerina <tobias.tejerina@gmail.com>
2025-06-16 15:29:02 -0300

    commit para guardarReporte

--- controller/QuestionController.php
+++ controller/QuestionController.php
@@ -11,5 +11,30 @@ class QuestionController extends BaseController
         $this->view = $view;
     }
 
+    public function showFormularioReporte($idPregunta){
+        {
+
+            $sql = "SELECT * FROM pregunta WHERE id = ?";
+            $pregunta = $this->database->query($sql, [$idPregunta]);
+
+            $nombreUsuario = $_SESSION['username'];
+
+            $data["user"] = $nombreUsuario;
+            $data["pregunta"] = $pregunta[0] ?? null;
+
+            if ($data["pregunta"]){
+                $this->view->render("formReport", $data);
+
+            }else{
+                $this->showError($this->view,'Error al buscar.', 'La pregunta o el usuario que solicitó no existen o han sido borrados.');
+            }
+        }
+
+
+        }
+
+    public function enviarReporte(){
+       // $this->model->guardarReporte(''$idUsuario,$idPregunta,$_POST['motivo']);
+    }
 
 }
--- model/QuestionModel.php
+++ model/QuestionModel.php
@@ -10,14 +10,24 @@ class QuestionModel
     }
 public function guardarReporte($idUsuario,$idPregunta,$motivo)
 {
-    if ($idUsuario !== null && $idPregunta !== null && $motivo !== null) {
+    if (!$this->alreadyReported($idUsuario, $idPregunta) && isset($motivo) && trim($motivo) !== '') {
         $sql = "INSERT INTO reporte_pregunta 
                     (id_pregunta, id_usuario, motivo, fecha_reporte, estado) 
                     VALUES (?, ?, ?, NOW(), false)";
-
         if ($this->database->execute($sql, [$idPregunta, $idUsuario, $motivo]))
             return true;
             }
             return false;
 }
-}
+    public function alreadyReported($idUsuario,$idPregunta)
+    {
+        if($idUsuario !== null && $idPregunta !== null){
+            $sql = "SELECT * FROM reporte_pregunta WHERE id_usuario = ? and id_pregunta = ?";
+        }else{
+            return true;
+        }
+
+        if ($this->database->query($sql, [$idUsuario, $idPregunta])) {return true;}
+        return false;
+    }
+ }
